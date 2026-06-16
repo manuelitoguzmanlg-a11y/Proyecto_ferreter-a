08668a706c02720e44ed3cae727aa27c99857c7b
manuelitoguzmanlg-a11y <manuelitoguzmanlg@gmail.com>
2026-06-15 23:22:13 -0600

    Create venta.py

---
+++ modulos/venta.py
@@ -0,0 +1,6 @@
+# modulos/venta.py 
+import streamlit as st 
+def mostrar_venta(): 
+# Aquí se muestra "Hola Mundo" 
+st.title("Hola Mundo") 
+st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
