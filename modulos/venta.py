da92403167c7e34beb0d914c6ffb7d81ef0bc109
manuelitoguzmanlg-a11y <manuelitoguzmanlg@gmail.com>
2026-06-16 15:37:44 -0600

    Update venta.py

--- modulos/venta.py
+++ modulos/venta.py
@@ -1,6 +1,5 @@
-# modulos/venta.py 
-import streamlit as st 
-def mostrar_venta(): 
-# Aquí se muestra "Hola Mundo" 
-st.title("Hola Mundo") 
-st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
+import streamlit as st
+
+def mostrar_venta():
+    st.title("Hola Mundo")
+    st.write("Este es un mensaje de prueba para verificar si la app está funcionando.")
