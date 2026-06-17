07dc248d6a78396eea322c4e820c09b4aa6cee5b
manuelitoguzmanlg-a11y <manuelitoguzmanlg@gmail.com>
2026-06-16 20:51:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # app.py
 import streamlit as st
-from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
-
-# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+from modulos.venta import mostrar_venta# Importamos la función mostrar_venta del módulo venta
+from modulos.login import login# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
 mostrar_venta()
