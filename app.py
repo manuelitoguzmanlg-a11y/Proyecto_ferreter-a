fdbc6cb9f6df9b782c5bcf169f061af9497953c0
manuelitoguzmanlg-a11y <manuelitoguzmanlg@gmail.com>
2026-06-19 12:20:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import streamlit as st
-
 from modulos.venta import mostrar_venta
 from modulos.login import login
 
-mostrar_venta()
-login()
+# Inicializar sesión
+if "sesion_iniciada" not in st.session_state:
+    st.session_state["sesion_iniciada"] = False
+
+# Si ya inició sesión
+if st.session_state["sesion_iniciada"]:
+
+    st.title("🏪 Sistema Ferretería Elohim")
+
+    st.success(
+        f"Bienvenido {st.session_state['usuario']} "
+        f"({st.session_state['tipo_usuario']})"
+    )
+
+    if st.button("Cerrar sesión"):
+        st.session_state.clear()
+        st.rerun()
+
+    mostrar_venta()
+
+# Si NO ha iniciado sesión
+else:
+    login()
