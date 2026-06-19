47a2830a5cff47374a1c34f527590ed0f5924011
manuelitoguzmanlg-a11y <manuelitoguzmanlg@gmail.com>
2026-06-19 12:29:29 -0600

    Update venta.py

--- modulos/venta.py
+++ modulos/venta.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 def mostrar_venta():
-    st.title("Hola Mundo")
-    st.write("Este es un mensaje de prueba para verificar si la app está funcionando.")
+    st.subheader("📦 Gestión de Productos")
+
+    st.text_input("Nombre del producto")
+    st.text_input("Código")
+    st.number_input("Precio", min_value=0.0)
+    st.number_input("Stock", min_value=0)
+
+    st.button("Guardar Producto")
